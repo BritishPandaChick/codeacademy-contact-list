8a245fcf85ffbe9496c595fcdd95878932559c63
BritishPandaChick <sbartley1080@gmail.com>
2019-05-14 21:00:24 -0400

    Update script file

--- js/script.js
+++ js/script.js
@@ -1,32 +1,29 @@
-var friends = new Object();
-
-
 var friends = {
-  bill: {
+bill: {
     firstName: "Bill",
     lastName: "Gates",
     number: "(206) 555-5555",
     address: ['One Microsoft Way', 'Redmond', 'WA', '98052']
-  },
-  steve: {
+    },
+steve: {
     firstName: "Steve",
     lastName: "Jobs",
-    number: "(206 455-5556)"
+    number: "(206) 455-5556",
     address: ['1989 Apple Road', 'Palo Alto', 'CA', '93948']
-  }
+    }
 };
 
 var list = function(friends) {
-  for (var key in friends) {
-    console.log(key);
-  }
+    for (var key in friends) {
+        console.log(key);
+    }
 };
 
 var search = function(name) {
-  for (var key in friends) {
-    if (friends[key].firstName === name) {
-      console.log(friends[key]);
-      return friends[key];
+    for (var key in friends) {
+        if (friends[key].firstName === name) {
+            console.log(friends[key]);
+            return friends[key];
+        }
     }
-  }
 };
